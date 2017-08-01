e2215146e6b1137525278e7995ff1de0b69a6ff5
Satya van Heummen <satya.vh@gmail.com>
2017-08-01 16:18:01 +0200

    Change main menu item

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -15,7 +15,7 @@ class MainMenu extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <Link className="nav-link" to="/">Milestones <span className="sr-only">(current)</span></Link>
+              <Link className="nav-link" to="/">Causes</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/profile">Profile</Link>
