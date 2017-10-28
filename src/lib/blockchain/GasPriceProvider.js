2a801472eea8ee2ef7379b8f33110a380adc4192
Casey Helbling <casey@softwareforgood.com>
2017-10-28 16:58:26 -0500

    eslint fixes

--- src/lib/blockchain/GasPriceProvider.js
+++ src/lib/blockchain/GasPriceProvider.js
@@ -1,18 +1,20 @@
 import SubProvider from 'web3-provider-engine/subproviders/subprovider';
 // import { utils } from "web3";
 
-//TODO use http://ethgasstation.info/json/ethgasAPI.json to get gas price
+// TODO use http://ethgasstation.info/json/ethgasAPI.json to get gas price
 export default class GasPriceProvider extends SubProvider {
   constructor(opts) {
     super();
     this.opts = opts || {};
   }
 
-  handleRequest(payload, next, end) {
-    if (payload.method !== 'eth_gasPrice') return next();
+  handleRequest(payload, next, _end) { // eslint-disable-line class-methods-use-this
+    if (payload.method !== 'eth_gasPrice') {
+      return next();
+    }
 
     // TODO re-enable this when deployed to mainnet
-    next();
+    return next();
     // end(null, utils.toHex(utils.toWei(4, 'gwei')));
   }
 }
