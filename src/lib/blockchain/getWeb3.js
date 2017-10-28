8b0118fee6c892c13f8c5b42169f0131616792cc
Casey Helbling <casey@softwareforgood.com>
2017-10-28 15:53:14 -0500

    eslint fixes

--- src/lib/blockchain/getWeb3.js
+++ src/lib/blockchain/getWeb3.js
@@ -1,38 +1,7 @@
 import Web3 from 'web3';
-import ZeroClientProvider from "./ZeroClientProvider";
+import ZeroClientProvider from './ZeroClientProvider';
 
-let givethWeb3 = undefined;
-
-const getWeb3 = () => {
-  return new Promise((resolve) => {
-
-    if (!givethWeb3) {
-      givethWeb3 = new Web3(process.env.REACT_APP_ETH_NODE_CONNECTION_URL);
-
-      // web3 1.0 expects the chainId to be no longer then 1 byte. If the chainId is longer then 1 byte,
-      // an error will be thrown. Testrpc by default uses the timestamp for the networkId, thus causing
-      // an error to be thrown. Here we override getId if necessary
-      // Since web3-eth-account account.js uses the following formula when signing a tx (Nat.toNumber(tx.chainId || "0x1") * 2 + 35),
-      // and that number is added to the signature.recoveryParam the max value the network ID can be is 110 (110 * 2 + 35 === 255) - recoveryParam
-      givethWeb3.eth.net.getId()
-        .then(id => {
-          if (id > 110) {
-            console.warn(`Web3 will throw errors when signing transactions if the networkId > 255 (1 byte).
-          networkID = ${id}. Overriding eth.net.getId() to return 100`);
-
-            givethWeb3.eth.net.getId = () => Promise.resolve(100);
-          }
-        });
-
-      givethWeb3.setWallet = setWallet;
-    }
-
-    resolve(givethWeb3);
-  })
-}
-
-export default getWeb3;
-
+let givethWeb3;
 /* ///////////// custom Web3 Functions ///////////// */
 
 function setWallet(wallet) {
@@ -42,7 +11,7 @@ function setWallet(wallet) {
     wsProvider: this.currentProvider,
     getAccounts: cb => cb(null, wallet.getAddresses()),
     approveTransaction: (txParams, cb) => {
-      //TODO handle locked wallet here?
+      // TODO handle locked wallet here?
       cb(null, true);
     },
     signTransaction: (txData, cb) => {
@@ -51,7 +20,7 @@ function setWallet(wallet) {
       const getId = (txData.chainId) ? Promise.resolve(txData.chainId) : this.eth.net.getId;
 
       getId()
-        .then(id => {
+        .then((id) => {
           txData.chainId = id;
 
           try {
@@ -60,23 +29,52 @@ function setWallet(wallet) {
           } catch (err) {
             cb(err);
           }
-
-        })
+        });
     },
   });
 
-  const getBalance = () => getWeb3()
-    .then(web3 => {
-      const addr = wallet.getAddresses()[ 0 ];
-
+  const getBalance = () => getWeb3() // eslint-disable-line no-use-before-define
+    .then((web3) => {
+      const addr = wallet.getAddresses()[0];
       return (addr) ? web3.eth.getBalance(addr) : undefined;
     })
-    .then(balance => wallet.balance = balance)
-    .catch(console.error);
+    .then(balance => wallet.balance = balance) // eslint-disable-line no-return-assign
+    .catch(console.error); // eslint-disable-line no-console
 
   getBalance();
-
   engine.on('block', getBalance);
-
   this.setProvider(engine);
 }
+
+const getWeb3 = () =>
+  new Promise((resolve) => {
+    if (!givethWeb3) {
+      givethWeb3 = new Web3(process.env.REACT_APP_ETH_NODE_CONNECTION_URL);
+
+      // web3 1.0 expects the chainId to be no longer then 1 byte. If the chainId is longer
+      // then 1 byte, an error will be thrown. Testrpc by default uses the timestamp for the
+      // networkId, thus causing an error to be thrown. Here we override getId if necessary
+      // Since web3-eth-account account.js uses the following formula when signing a tx
+      // (Nat.toNumber(tx.chainId || "0x1") * 2 + 35), and that number is added to the
+      // signature.recoveryParam the max value the network ID can be is
+      // 110 (110 * 2 + 35 === 255) - recoveryParam
+      givethWeb3.eth.net.getId()
+        .then((id) => {
+          if (id > 110) {
+            const msg = `Web3 will throw errors when signing transactions if the networkId > 255 (1 byte).
+          networkID = ${id}. Overriding eth.net.getId() to return 100`;
+
+            console.warn(msg); // eslint-disable-line no-console
+
+            givethWeb3.eth.net.getId = () => Promise.resolve(100);
+          }
+        });
+
+      givethWeb3.setWallet = setWallet;
+    }
+
+    resolve(givethWeb3);
+  });
+
+export default getWeb3;
+
