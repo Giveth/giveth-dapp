9132c7b2e8331369064474070142d45c04242b45
Casey Helbling <casey@softwareforgood.com>
2017-10-28 12:27:57 -0500

    update App.jsx for linter rules

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import './styles/App.css';
+// function Listing({ hello }) {
+//   return <div>{hello}</div>;
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src="./assets/logo.svg" className="App-logo" alt="Giveth Unicorn Logo" />
+        <h2>Welcome to Giveth</h2>
+      </div>
+      <p className="App-intro">
+        To get started, get some &#926; and save the world.
+      </p>
+    </div>
+  );
+}
+
+export default App;
