de02a85def23dab46da2e1babeb587f1de9a896a
Vojtech Simetka <vojtech@simetka.cz>
2017-11-17 20:10:24 -0500

    Not found page linting erros gone, rewritten to pure component.

---
+++ src/components/views/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Route not found page
+ */
+const NotFound = () => (
+  <center>
+    <h1>You&apos;ve reached the edge of the universe</h1>
+  </center>
+);
+
+export default NotFound;
