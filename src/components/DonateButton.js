f96ef4658c78bcdbd3cd690bbf05a868feea5d8e
Vojtech Simetka <vojtech@simetka.cz>
2017-11-17 20:05:43 -0500

    Fixed ViewDAC component linting errors.

--- src/components/DonateButton.js
+++ src/components/DonateButton.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import SkyLight from 'react-skylight'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SkyLight from 'react-skylight';
 import { utils } from 'web3';
 import getNetwork from '../lib/blockchain/getNetwork';
 
-import { feathersClient } from '../lib/feathersClient'
+import { feathersClient } from '../lib/feathersClient';
 import { Form, Input } from 'formsy-react-components';
-import { takeActionAfterWalletUnlock } from '../lib/middleware'
-import currentUserModel from '../models/currentUserModel'
-import { displayTransactionError } from '../lib/helpers'
+import { takeActionAfterWalletUnlock } from '../lib/middleware';
+import currentUserModel from '../models/currentUserModel';
+import { displayTransactionError } from '../lib/helpers';
 
 class DonateButton extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       isSaving: false,
       formIsValid: false,
-      amount: ""
-    }
+      amount: '',
+    };
 
-    this.submit = this.submit.bind(this)
+    this.submit = this.submit.bind(this);
   }
 
-  openDialog(){
-    if(this.props.currentUser) {
-      takeActionAfterWalletUnlock(this.props.wallet, () => this.refs.donateDialog.show())
+  openDialog() {
+    if (this.props.currentUser) {
+      takeActionAfterWalletUnlock(this.props.wallet, () => this.refs.donateDialog.show());
     } else {
       React.swal({
-        title: "You're almost there...", 
-        content: React.swal.msg(
-          <p>
+        title: "You're almost there...",
+        content: React.swal.msg(<p>
             Great to see that you want to donate!! However you first need to sign up (or sign in).
             Also make sure to transfer some Ether to your Giveth wallet before donating.
-          </p>
-        ),
+                                </p>),
         icon: 'info',
-        buttons: ["Cancel", "Sign up now!"]
+        buttons: ['Cancel', 'Sign up now!'],
       }).then((isConfirmed) => {
-        if(isConfirmed) this.props.history.push('/signup')
-      });      
+        if (isConfirmed) this.props.history.push('/signup');
+      });
     }
   }
 
-  focusInput(){
-    this.refs.amount.element.focus()
+  focusInput() {
+    this.refs.amount.element.focus();
   }
 
   toggleFormValid(state) {
-    this.setState({ formIsValid: state })
+    this.setState({ formIsValid: state });
   }
 
 
@@ -73,53 +71,51 @@ class DonateButton extends Component {
           delegateId: this.props.model._id,
           owner: this.props.currentUser.giverId || 0,
           ownerId: this.props.currentUser,
-          ownerType: 'giver'
+          ownerType: 'giver',
         });
       } else {
         Object.assign(donation, {
           owner: this.props.model.adminId,
           ownerId: this.props.model._id,
-          ownerType: this.props.type.toLowerCase()
-        })
+          ownerType: this.props.type.toLowerCase(),
+        });
       }
 
       return service.create(donation)
-        .then(() =>{
+        .then(() => {
           this.setState({
             isSaving: false,
-            amount: 10
+            amount: 10,
           });
 
-        // For some reason (I suspect a rerender when donations are being fetched again)
-        // the skylight dialog is sometimes gone and this throws error
-        if(this.refs.donateDialog) this.refs.donateDialog.hide()
-        
-        let msg;
-        if(this.props.type === "DAC") {
-          msg = React.swal.msg(
-            <div>
-              <p> 
+          // For some reason (I suspect a rerender when donations are being fetched again)
+          // the skylight dialog is sometimes gone and this throws error
+          if (this.refs.donateDialog) this.refs.donateDialog.hide();
+
+          let msg;
+          if (this.props.type === 'DAC') {
+            msg = React.swal.msg(<div>
+              <p>
                 You're donation is pending, <a href={`${etherScanUrl}tx/${txHash}`} target="_blank" rel="noopener noreferrer">view the transaction here.</a>
-                You have full control of this donation and <strong>can take it back at any time</strong>. 
+                You have full control of this donation and <strong>can take it back at any time</strong>.
                 You will also have a <strong>3 day window</strong> to veto the use of these funds upon delegation by the dac.
               </p>
               <p>Do make sure to <a href={this.props.commmunityUrl} target="_blank" rel="noopener noreferrer">join the community</a> to follow the progress of this DAC.</p>
-            </div>)
-        } else {
-          msg = React.swal.msg(
-            <div>
+                                 </div>);
+          } else {
+            msg = React.swal.msg(<div>
               <p>You're donation is pending.</p>
               <p>Do make sure to <a href={this.props.commmunityUrl} target="_blank" rel="noopener noreferrer">join the community</a> to follow the progress of this DAC.</p>
-            </div>)
-        }
+                                 </div>);
+          }
 
-        React.swal({
-          title: "You're awesome!", 
-          content: msg,
-          icon: 'success',
-        })
-      });
-    }
+          React.swal({
+            title: "You're awesome!",
+            content: msg,
+            icon: 'success',
+          });
+        });
+    };
 
     let txHash;
     let etherScanUrl;
@@ -129,48 +125,57 @@ class DonateButton extends Component {
         etherScanUrl = network.etherscan;
 
         return liquidPledging.donate(this.props.currentUser.giverId || 0, this.props.model.adminId, { value: amount })
-          .once('transactionHash', hash => {
+          .once('transactionHash', (hash) => {
             txHash = hash;
             donate(etherScanUrl, txHash);
           });
       })
       .then(() => {
-        React.toast.success(<p>Your donation has been confirmed!<br/><a href={`${etherScanUrl}tx/${txHash}`} target="_blank" rel="noopener noreferrer">View transaction</a></p>)
+        React.toast.success(<p>Your donation has been confirmed!<br /><a href={`${etherScanUrl}tx/${txHash}`} target="_blank" rel="noopener noreferrer">View transaction</a></p>);
       }).catch((e) => {
         console.log(e);
-        displayTransactionError(txHash, etherScanUrl)
+        displayTransactionError(txHash, etherScanUrl);
 
         this.setState({ isSaving: false });
-      })
+      });
   }
 
 
   render() {
-    const { type, model, wallet } = this.props
-    let { isSaving, amount, formIsValid } = this.state;
+    const { type, model, wallet } = this.props;
+    const { isSaving, amount, formIsValid } = this.state;
     const style = {
-      display: 'inline-block'     
-    }
+      display: 'inline-block',
+    };
 
-    return(
+    return (
       <span style={style}>
         <a className="btn btn-success" onClick={() => this.openDialog()}>
           Donate
         </a>
 
         {wallet &&
-          <SkyLight hideOnOverlayClicked ref="donateDialog" title={`Support this ${type}!`}
-                    afterOpen={() => this.focusInput()}>
+          <SkyLight
+            hideOnOverlayClicked
+            ref="donateDialog"
+            title={`Support this ${type}!`}
+            afterOpen={() => this.focusInput()}
+          >
             <strong>Give Ether to support <em>{model.title}</em></strong>
 
-            {[ "DAC", "campaign" ].indexOf(type) > -1 &&
+            {['DAC', 'campaign'].indexOf(type) > -1 &&
             <p>Pledge: as long as the {type} owner does not lock your money you can take it back any time.</p>
             }
 
             <p>Your wallet balance: <em>&#926;{wallet.getBalance()}</em></p>
 
-            <Form onSubmit={this.submit} mapping={this.mapInputs} onValid={() => this.toggleFormValid(true)}
-                  onInvalid={() => this.toggleFormValid(false)} layout='vertical'>
+            <Form
+              onSubmit={this.submit}
+              mapping={this.mapInputs}
+              onValid={() => this.toggleFormValid(true)}
+              onInvalid={() => this.toggleFormValid(false)}
+              layout="vertical"
+            >
               <div className="form-group">
                 <Input
                   name="amount"
@@ -182,35 +187,35 @@ class DonateButton extends Component {
                   placeholder="10"
                   validations={{
                     lessThan: wallet.getBalance() - 0.5,
-                    greaterThan: 0.1
+                    greaterThan: 0.1,
                   }}
                   validationErrors={{
                     greaterThan: 'Minimum value must be at least Ξ0.1',
-                    lessThan: 'This donation exceeds your wallet balance. Pledge that you also need to pay for the transaction.'
+                    lessThan: 'This donation exceeds your wallet balance. Pledge that you also need to pay for the transaction.',
                   }}
                   required
                 />
               </div>
 
-              <button className="btn btn-success" formNoValidate={true} type="submit" disabled={isSaving || !formIsValid}>
-                {isSaving ? "Saving..." : "Donate Ξ"}
+              <button className="btn btn-success" formNoValidate type="submit" disabled={isSaving || !formIsValid}>
+                {isSaving ? 'Saving...' : 'Donate Ξ'}
               </button>
             </Form>
 
           </SkyLight>
         }
       </span>
-    )
+    );
   }
 }
 
-export default DonateButton
+export default DonateButton;
 
 DonateButton.propTypes = {
   type: PropTypes.string.isRequired,
   model: PropTypes.shape({
-    adminId: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired,
+    adminId: PropTypes.string,
+    _id: PropTypes.string,
     title: PropTypes.string.isRequired,
   }).isRequired,
   currentUser: currentUserModel,
@@ -218,5 +223,5 @@ DonateButton.propTypes = {
   wallet: PropTypes.shape({
     unlocked: PropTypes.bool.isRequired,
     lock: PropTypes.func.isRequired,
-  })
+  }),
 };
---
+++ src/components/views/ViewDAC.jsx
@@ -0,0 +1,216 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Avatar from 'react-avatar';
+
+import { feathersClient } from '../../lib/feathersClient';
+import Loader from '../Loader';
+import GoBackButton from '../GoBackButton';
+import BackgroundImageHeader from '../BackgroundImageHeader';
+import DonateButton from '../DonateButton';
+import ShowTypeDonations from '../ShowTypeDonations';
+import CommunityButton from '../CommunityButton';
+import currentUserModel from '../../models/currentUserModel';
+import CampaignCard from '../CampaignCard';
+import { getUserName, getUserAvatar } from '../../lib/helpers';
+
+
+/**
+ * The DAC detail view mapped to /campaing/id
+ *
+ * @param currentUser  Currently logged in user information
+ * @param history      Browser history object
+ * @param wallet       Wallet object with the balance and all keystores
+ */
+class ViewDAC extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      isLoadingDonations: true,
+      donations: [],
+      isLoadingCampaigns: true,
+      campaigns: [],
+    };
+  }
+
+  componentDidMount() {
+    const dacId = this.props.match.params.id;
+
+    feathersClient.service('dacs').find({ query: { _id: dacId } })
+      .then((resp) => {
+        this.setState(Object.assign({}, resp.data[0], {
+          isLoading: false,
+          hasError: false,
+        }));
+      })
+      .catch(() =>
+        this.setState({ isLoading: false, id: dacId }));
+
+    // lazy load donations
+    this.donationsObserver = feathersClient.service('donations/history').watch({ listStrategy: 'always' }).find({
+      query: {
+        delegateId: dacId,
+        $sort: { createdAt: -1 },
+      },
+    }).subscribe(
+      (resp) => {
+        this.setState({
+          donations: resp.data,
+          isLoadingDonations: false,
+        });
+      },
+      () => this.setState({ isLoadingDonations: false }),
+    );
+
+    // lazy load campaigns
+    this.campaignsObserver = feathersClient.service('campaigns').watch({ strategy: 'always' }).find({
+      query: {
+        projectId: {
+          $gt: '0', // 0 is a pending campaign
+        },
+        dacs: dacId,
+        $limit: 200,
+      },
+    }).subscribe(
+      (resp) => {
+        this.setState({ campaigns: resp.data, isLoadingCampaigns: false });
+      },
+      () => {
+        this.setState({ isLoadingCampaigns: false });
+      },
+    );
+  }
+
+  componentWillUnmount() {
+    // this.donationsObserver.unsubscribe()
+    this.campaignsObserver.unsubscribe();
+  }
+
+  render() {
+    const { wallet, history, currentUser } = this.props;
+    const {
+      isLoading, id, delegateId, title, description, image, owner, donations,
+      isLoadingDonations, communityUrl, campaignsCount, isLoadingCampaigns, campaigns,
+    } = this.state;
+
+    return (
+      <div id="view-cause-view">
+        { isLoading &&
+          <Loader className="fixed" />
+        }
+
+        { !isLoading &&
+          <div>
+            <BackgroundImageHeader image={image} height={300} >
+              <h6>Decentralized Altruistic Community</h6>
+              <h1>{title}</h1>
+
+              <DonateButton
+                type="DAC"
+                model={{ title, _id: id, adminId: delegateId }}
+                wallet={wallet}
+                currentUser={currentUser}
+                commmunityUrl={communityUrl}
+                history={history}
+              />
+              {communityUrl &&
+                <CommunityButton
+                  className="btn btn-secondary"
+                  url={communityUrl}
+                >&nbsp;Join our community
+                </CommunityButton>
+              }
+            </BackgroundImageHeader>
+
+            <div className="container-fluid">
+
+              <div className="row">
+                <div className="col-md-8 m-auto">
+
+                  <GoBackButton history={history} />
+
+                  <center>
+                    <Link to={`/profile/${owner.address}`}>
+                      <Avatar size={50} src={getUserAvatar(owner)} round />
+                      <p className="small">{getUserName(owner)}</p>
+                    </Link>
+                  </center>
+
+                  <div className="card content-card">
+                    <div className="card-body content">
+                      <div dangerouslySetInnerHTML={{ __html: description }}>
+                        { /* TODO: Find an alternative to dangerouslySetInnerHTML */ }
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row spacer-top-50 spacer-bottom-50">
+                <div className="col-md-8 m-auto card-view">
+                  <h4>{campaignsCount} campaign(s)</h4>
+                  <p>These campaigns are working hard to solve the cause of this DAC</p>
+
+                  { campaignsCount > 0 && isLoadingCampaigns &&
+                    <Loader className="small" />
+                  }
+
+                  { campaignsCount > 0 && !isLoadingCampaigns &&
+                    <div className="card-deck">
+                      { campaigns.map(c =>
+
+                        (<CampaignCard
+                          key={c._id} // eslint-disable-line no-underscore-dangle
+                          campaign={c}
+                          currentUser={currentUser}
+                          wallet={wallet}
+                          history={history}
+                        />))}
+                    </div>
+                  }
+
+                </div>
+              </div>
+
+              <div className="row spacer-top-50 spacer-bottom-50">
+                <div className="col-md-8 m-auto">
+                  <h4>Donations</h4>
+                  <ShowTypeDonations donations={donations} isLoading={isLoadingDonations} />
+                  <DonateButton
+                    type="DAC"
+                    model={{ title, _id: id, adminId: delegateId }}
+                    wallet={wallet}
+                    currentUser={currentUser}
+                    history={history}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+ViewDAC.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  currentUser: currentUserModel,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+  wallet: PropTypes.shape({}).isRequired,
+};
+
+ViewDAC.defaultProps = {
+  currentUser: {},
+};
+
+export default ViewDAC;
