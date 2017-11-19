55570fa78a9f4ab904226045fa04b5529178df44
Vojtech Simetka <vojtech@simetka.cz>
2017-11-19 18:53:14 -0500

    Fixed linting issues in CommunityButton.

---
+++ src/components/CommunityButton.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CommunityButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      icon: 'external-link',
+    };
+  }
+
+  componentWillMount() {
+    // parse community icon from url
+
+    const u = this.props.url;
+
+    if (u.indexOf('slack') > -1) this.setState({ icon: 'fa-slack' });
+    if (u.indexOf('reddit') > -1) this.setState({ icon: 'fa-reddit' });
+    if (u.indexOf('facebook') > -1) this.setState({ icon: 'fa-facebook-square' });
+    if (u.indexOf('github') > -1) this.setState({ icon: 'fa-github' });
+    if (u.indexOf('twitter') > -1) this.setState({ icon: 'fa-twitter' });
+    if (u.indexOf('linkedin') > -1) this.setState({ icon: 'fa-linkedin' });
+  }
+
+  render() {
+    return (
+      <a
+        className={this.props.className}
+        href={this.props.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className={`fa ${this.state.icon}`} />
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+CommunityButton.propTypes = {
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+CommunityButton.defaultProps = {
+  className: '',
+};
+
+export default CommunityButton;
