d8fb450baf5d3ccdf7fc4af524946b6fd1660cf2
Vojtech Simetka <vojtech@simetka.cz>
2017-11-20 11:54:06 -0500

    Fixed linting errors in MainMenu component.

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,55 +1,62 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Avatar from 'react-avatar';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-import { Link, NavLink } from 'react-router-dom'
-import {withRouter} from "react-router-dom";
-
-import AuthenticatedLink from './AuthenticatedLink'
-import AuthenticatedNavLink from './AuthenticatedNavLink'
-
-import Avatar from 'react-avatar'
-
-import currentUserModel from '../models/currentUserModel'
+import AuthenticatedLink from './AuthenticatedLink';
+import AuthenticatedNavLink from './AuthenticatedNavLink';
+import currentUserModel from '../models/currentUserModel';
 
 
 /**
-  The main top menu
-**/
-
+ * The main top menu
+ *
+ * @param currentUser  Currently logged in user information
+ * @param history      Browser history object
+ * @param wallet       Wallet object with the balance and all keystores
+ */
 class MainMenu extends Component {
   constructor(props) {
     super();
 
     this.state = {
       walletLocked: props.wallet ? !props.wallet.unlocked : true,
-      showMobileMenu: false
-    }
+      showMobileMenu: false,
+    };
+
+    this.lockWallet = this.lockWallet.bind(this);
+    this.signout = this.signout.bind(this);
+  }
+
+  componentDidMount() {
+    // when route changes, close the menu
+    this.props.history.listen(() => this.setState({ showMobileMenu: false }));
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.wallet && nextProps.wallet.unlocked && this.state.walletLocked) {
       this.setState({
-        walletLocked: false
+        walletLocked: false,
       });
     }
   }
 
   signout() {
     this.props.onSignOut();
-    this.props.history.push('/')
+    this.props.history.push('/');
   }
 
-  lockWallet = e => {
+  lockWallet(e) {
     e.preventDefault();
 
     React.swal({
-      title: "Lock your wallet?",
+      title: 'Lock your wallet?',
       text: "You will be redirected to the home page. Any changes you're making will be lost.",
-      icon: "warning",
-      dangerMode: true,     
-      buttons: ["Cancel", "Yes, lock wallet!"]
-    }).then( (isConfirmed) => {
-      if(isConfirmed) {
+      icon: 'warning',
+      dangerMode: true,
+      buttons: ['Cancel', 'Yes, lock wallet!'],
+    }).then((isConfirmed) => {
+      if (isConfirmed) {
         this.props.wallet.lock();
         this.setState({ walletLocked: true });
         this.props.history.push('/');
@@ -57,32 +64,29 @@ class MainMenu extends Component {
     });
   }
 
-  toggleMobileMenu(){
-    this.setState({ showMobileMenu: !this.state.showMobileMenu })
-  }
-
-  componentDidMount(){
-    // when route changes, close the menu
-    this.props.history.listen((location, action) => {
-      this.setState({ showMobileMenu: false })
-    })
+  toggleMobileMenu() {
+    this.setState({ showMobileMenu: !this.state.showMobileMenu });
   }
 
   render() {
-    const { currentUser, wallet } = this.props
-    const { showMobileMenu } = this.state
+    const { currentUser, wallet } = this.props;
+    const { showMobileMenu } = this.state;
 
     return (
       <nav id="main-menu" className={`navbar navbar-expand-lg fixed-top ${showMobileMenu ? 'show' : ''} `}>
-        <button className="navbar-toggler navbar-toggler-right" type="button" onClick={()=>this.toggleMobileMenu()}>
-          <i className={`navbar-toggler-icon fa ${showMobileMenu ? 'fa-close' : 'fa-bars'}`}></i>
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          onClick={() => this.toggleMobileMenu()}
+        >
+          <i className={`navbar-toggler-icon fa ${showMobileMenu ? 'fa-close' : 'fa-bars'}`} />
         </button>
 
         <ul className="navbar-nav mobile-wallet-lock">
           { this.props.currentUser && this.props.wallet && this.state.walletLocked &&
             <li className="nav-item mr-sm-2">
               <AuthenticatedNavLink className="nav-link" to="#">
-                <i className="fa fa-lock"></i>
+                <i className="fa fa-lock" />
                 Wallet
               </AuthenticatedNavLink>
             </li>
@@ -90,31 +94,49 @@ class MainMenu extends Component {
           { this.props.currentUser && this.props.wallet && !this.state.walletLocked &&
             <li className="nav-item mr-sm-2">
               <NavLink className="nav-link" to="#" onClick={this.lockWallet}>
-                <i className="fa fa-unlock"></i>
+                <i className="fa fa-unlock" />
                 Wallet
               </NavLink>
             </li>
           }
-        </ul>  
+        </ul>
 
         <Link className="navbar-brand" to="/">
           <img src="/img/Giveth-typelogo.svg" width="70px" alt="Giveth logo" />
         </Link>
 
 
-
         <div className={`collapse navbar-collapse ${showMobileMenu ? 'show' : ''} `} id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/dacs" activeClassName="active">Communities</NavLink>
+              <NavLink
+                className="nav-link"
+                to="/dacs"
+                activeClassName="active"
+              >Communities
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/campaigns" activeClassName="active">Campaigns</NavLink>
+              <NavLink
+                className="nav-link"
+                to="/campaigns"
+                activeClassName="active"
+              >Campaigns
+              </NavLink>
             </li>
 
             {this.props.currentUser &&
               <li className="nav-item dropdown">
-                <NavLink className="nav-link dropdown-toggle" id="navbarDropdownDashboard" to="/dashboard" activeClassName="active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dashboard</NavLink>
+                <NavLink
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdownDashboard"
+                  to="/dashboard"
+                  activeClassName="active"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >Dashboard
+                </NavLink>
                 <div className={`dropdown-menu ${showMobileMenu ? 'show' : ''} `} aria-labelledby="navbarDropdownDashboard">
                   <Link className="dropdown-item" to="/donations">Donations</Link>
                   <Link className="dropdown-item" to="/delegations">Delegations</Link>
@@ -131,7 +153,7 @@ class MainMenu extends Component {
             { this.props.currentUser && this.props.wallet && this.state.walletLocked &&
             <li className="nav-item mr-sm-2">
               <AuthenticatedNavLink className="nav-link" to="#">
-                <i className="fa fa-lock"></i>
+                <i className="fa fa-lock" />
                 &nbsp;UnLock Wallet
               </AuthenticatedNavLink>
             </li>
@@ -139,7 +161,7 @@ class MainMenu extends Component {
             { this.props.currentUser && this.props.wallet && !this.state.walletLocked &&
               <li className="nav-item mr-sm-2">
                 <NavLink className="nav-link" to="#" onClick={this.lockWallet}>
-                  <i className="fa fa-unlock"></i>
+                  <i className="fa fa-unlock" />
                   &nbsp;Lock Wallet
                 </NavLink>
               </li>
@@ -157,17 +179,24 @@ class MainMenu extends Component {
               <NavLink className="nav-link" to="/signin" activeClassName="active">Sign In</NavLink>
             }
             { !this.props.currentUser &&
-              <NavLink className="nav-link" to="/signup" activeClassName="active">Sign Up</NavLink>              
+              <NavLink className="nav-link" to="/signup" activeClassName="active">Sign Up</NavLink>
             }
 
             { this.props.currentUser &&
               <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" id="navbarDropdownYou" to="/" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdownYou"
+                  to="/"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
                   { currentUser && currentUser.avatar &&
-                    <Avatar className="menu-avatar" size={30} src={currentUser.avatar} round={true}/>                  
+                    <Avatar className="menu-avatar" size={30} src={currentUser.avatar} round />
                   }
 
-                  { currentUser && currentUser.name && 
+                  { currentUser && currentUser.name &&
                     <span>{currentUser.name}</span>
                   }
 
@@ -175,10 +204,23 @@ class MainMenu extends Component {
                     <span>Hi, you!</span>
                   }
                 </Link>
-                <div className={`dropdown-menu dropdown-profile ${showMobileMenu ? 'show' : ''}`} aria-labelledby="navbarDropdownYou">
-                  <AuthenticatedLink className="dropdown-item" to="/profile" wallet={wallet}>Profile</AuthenticatedLink>
-                  <AuthenticatedLink className="dropdown-item" to="/wallet" wallet={wallet}>Wallet</AuthenticatedLink>
-                  <a className="dropdown-item" onClick={()=>this.signout()}>Sign out</a>
+                <div
+                  className={`dropdown-menu dropdown-profile ${showMobileMenu ? 'show' : ''}`}
+                  aria-labelledby="navbarDropdownYou"
+                >
+                  <AuthenticatedLink
+                    className="dropdown-item"
+                    to="/profile"
+                    wallet={wallet}
+                  >Profile
+                  </AuthenticatedLink>
+                  <AuthenticatedLink
+                    className="dropdown-item"
+                    to="/wallet"
+                    wallet={wallet}
+                  >Wallet
+                  </AuthenticatedLink>
+                  <button className="dropdown-item" onClick={this.signout}>Sign out</button>
                 </div>
               </li>
             }
@@ -186,11 +228,11 @@ class MainMenu extends Component {
 
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default withRouter(MainMenu)
+export default withRouter(MainMenu);
 
 MainMenu.propTypes = {
   currentUser: currentUserModel,
@@ -198,5 +240,15 @@ MainMenu.propTypes = {
     unlocked: PropTypes.bool.isRequired,
     lock: PropTypes.func.isRequired,
   }),
-  onSignOut: PropTypes.func.isRequired
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+    listen: PropTypes.func.isRequired,
+  }).isRequired,
+  onSignOut: PropTypes.func.isRequired,
+};
+
+MainMenu.defaultProps = {
+  currentUser: undefined,
+  wallet: undefined,
 };
