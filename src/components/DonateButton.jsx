cbf6d1c08f2985fb62ceed5b0bd754b790ab1467
Matt Stabeler <matt@bigbadweb.co.uk>
2017-12-22 15:11:17 +0000

    Check gas price when validatin donation amount

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -263,11 +263,11 @@ class DonateButton extends Component {
                   onChange={(name, value) => this.setState({ mewAmount: value })}
                   placeholder="10"
                   validations={{
-                    lessThan: wallet.getBalance(),
+                    lessThan: wallet.getBalance() + (gas * 1000000000),
                     greaterEqualThan: 0.1,
                   }}
                   validationErrors={{
-                    lessThan: 'This donation exceeds your Giveth wallet balance. Please top up your wallet or donate with MyEtherWallet.',
+                    lessThan: 'This donation exceeds your Giveth wallet balance (plus gas price). Please top up your wallet or donate with MyEtherWallet.',
                     greaterEqualThan: 'Minimum value must be at least Ξ0.1',
                   }}
                   required
