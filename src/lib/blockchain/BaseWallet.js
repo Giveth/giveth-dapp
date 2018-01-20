f0461cc8837abdbf06b437ab2b0f8476b9af0859
ojones <wakeupoj@gmail.com>
2018-01-19 22:23:08 -0800

    create dac works with harcoded private key

--- src/lib/blockchain/BaseWallet.js
+++ src/lib/blockchain/BaseWallet.js
@@ -1,15 +1,26 @@
 import { utils } from 'web3';
+import Accounts from 'web3-eth-accounts';
 
 class BaseWallet {
   /**
    * @param provider      optional. This is necessary when signing a transaction to
    *                      retrieve chainId, gasPrice, and nonce automatically
    */
-  constructor() {
+  constructor(provider) {
+    // these are harcoded defaults
     this.unlocked = true;
     this.balance = '10000000000000000000';
     this.address = '0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0';
     this.keystores = [''];
+    // this sets up the web3 account (not super sure we need this for metamask)
+    this.accounts = new Accounts(provider);
+    // this the private key provided by "testrpc -d"
+    // meta mask and status will keep key, not us
+    // so probably need to stop depending on this next line
+    // to add accout to wallet
+    // need to prepend the "0x" for it to work
+    const testprcAddress1 = this.accounts.wallet.add('0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1');
+    debugger;
   }
 
   /**
@@ -46,15 +57,10 @@ class BaseWallet {
    * @returns   signature object. https://web3js.readthedocs.io/en/1.0/web3-eth-accounts.html#signtransaction
    */
   signTransaction(txData) {
-    if (!this.unlocked) throw new Error('Locked Wallet');
-
-    // TODO: web3 version of this wallet code
-
-    // if (!txData.gasPrice || !txData.nonce || !txData.chainId) throw new Error('gasPrice, nonce, and chainId are required');
-
-    // const accounts = mapGet.call(mapAccounts, this);
-
-    // return accounts.wallet[0].signTransaction(txData);
+    if (!txData.gasPrice || !txData.nonce || !txData.chainId) throw new Error('gasPrice, nonce, and chainId are required');
+    debugger;
+    // here is another place where we want meta mask to take over
+    return this.accounts.wallet[0].signTransaction(txData);
   }
 
   /**
