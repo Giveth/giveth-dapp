25ce218dc177a9975fdfa5d98b4e24b9e6070fc2
Satya van Heummen <satya.vh@gmail.com>
2018-04-25 11:03:04 +0200

    Campaigns can now be created

--- src/lib/blockchain/getNetwork.js
+++ src/lib/blockchain/getNetwork.js
@@ -1,4 +1,4 @@
-// import { LiquidPledging } from 'giveth-liquidpledging';
+import { LiquidPledging } from 'giveth-liquidpledging';
 import { LPPDacFactory } from 'lpp-dac';
 import { LPPCampaignFactory } from 'lpp-campaign';
 import { LPPCappedMilestoneFactory } from 'lpp-capped-milestone';
@@ -13,13 +13,15 @@ export default () => {
 
   return getWeb3().then(web3 => {
     network = Object.assign({}, config);
-    // network.liquidPledging = new LiquidPledging(web3, network.liquidPledgingAddress);
+    network.liquidPledging = new LiquidPledging(web3, network.liquidPledgingAddress);
     network.lppDacFactory = new LPPDacFactory(web3, network.lppDacFactoryAddress);
-    network.LPPCampaignFactory = new LPPCampaignFactory(web3, network.lppCampaignFactoryAddress);
-    network.LPPCappedMilestoneFactory = new LPPCappedMilestoneFactory(
+    network.lppCampaignFactory = new LPPCampaignFactory(web3, network.lppCampaignFactoryAddress);
+    network.lppCappedMilestoneFactory = new LPPCappedMilestoneFactory(
       web3,
       network.lppCappedMilestoneFactoryAddress,
     );
+
+    console.log(network)
 
     return network;
   });
