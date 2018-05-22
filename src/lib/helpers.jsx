22985a59565f5cc25e6c24201cee7b44e63600ef
perissology <perissology@protonmail.com>
2018-05-22 08:39:29 -0700

    fix bug that breaks signin when feathers data is cleared

--- src/lib/helpers.jsx
+++ src/lib/helpers.jsx
@@ -18,18 +18,24 @@ export const authenticate = wallet => {
     address: wallet.getAddresses()[0],
   };
 
-  return new Promise((resolve, reject) => {
-    feathersClient.authenticate(authData).catch(response => {
-      // normal flow will issue a 401 with a challenge message we need to sign and send to
-      // verify our identity
-      if (response.code === 401 && response.data.startsWith('Challenge =')) {
-        const msg = response.data.replace('Challenge =', '').trim();
+  feathersClient.passport
+    .getJWT()
+    .then(accessToken => {
+      if (accessToken) return feathersClient.logout();
+      return undefined;
+    })
+    .then(() =>
+      feathersClient.authenticate(authData).catch(response => {
+        // normal flow will issue a 401 with a challenge message we need to sign and send to
+        // verify our identity
+        if (response.code === 401 && response.data.startsWith('Challenge =')) {
+          const msg = response.data.replace('Challenge =', '').trim();
 
-        return resolve(wallet.signMessage(msg).signature);
-      }
-      return reject(response);
-    });
-  })
+          return wallet.signMessage(msg).signature;
+        }
+        throw new Error(response);
+      }),
+    )
     .then(signature => {
       authData.signature = signature;
       return feathersClient.authenticate(authData);
