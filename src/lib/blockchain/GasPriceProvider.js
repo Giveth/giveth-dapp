86b778d7a8328e18aeaa414e99ca1dd6d4558f12
perissology <perissology@protonmail.com>
2018-06-06 17:53:36 -0700

    only use gasPriceService on mainnet

--- src/lib/blockchain/GasPriceProvider.js
+++ src/lib/blockchain/GasPriceProvider.js
@@ -6,21 +6,38 @@ export default class GasPriceProvider extends SubProvider {
   constructor(opts) {
     super();
     this.opts = opts || {};
+    this.netLookups = [];
   }
 
   /* eslint-disable class-methods-use-this */
   handleRequest(payload, next, end) {
     // eslint-disable-line class-methods-use-this
-    if (payload.method !== 'eth_gasPrice') {
+    if (payload.method !== 'eth_gasPrice' || this.netLookups.includes(payload.id)) {
       next();
       return;
     }
 
-    getGasPrice()
-      .then(gasPrice => {
-        end(null, utils.toHex(gasPrice));
-      })
-      .catch(() => next());
+    const id = new Date().getTime();
+    this.netLookups.push(id);
+
+    this.engine.sendAsync(
+      { jsonrpc: '2.0', id, method: 'net_version', params: [] },
+      (err, response) => {
+        const i = this.netLookups.findIndex(lId => lId === response.id);
+        if (i > -1) this.netLookups.splice(i, 1);
+
+        // only call getGasPrice for mainnet b/c it uses ethgasstation api
+        if (response.result === '1') {
+          getGasPrice()
+            .then(gasPrice => {
+              end(null, utils.toHex(gasPrice));
+            })
+            .catch(() => next());
+        } else {
+          next();
+        }
+      },
+    );
   }
   /* eslint-enable class-methods-use-this */
 }
