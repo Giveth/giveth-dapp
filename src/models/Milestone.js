be2e56ba9a5171e54a92ff94a1c7fb2d16babef8
Amin Latifi <a.latifi.al@gmail.com>
2020-04-09 17:53:21 +0430

    Milestones founded (partially or completely) cannot be edited. * Campaign co-owner can edit milestone too. * Fixes #1118

--- src/models/Milestone.js
+++ src/models/Milestone.js
@@ -627,13 +627,17 @@ export default class Milestone extends BasicModel {
   canUserEdit(user) {
     return (
       user &&
-      [this.ownerAddress, this.campaign.ownerAddress].includes(user.address) &&
+      user.address &&
+      [this.ownerAddress, this.campaign.ownerAddress, this.campaign.coownerAddress].includes(
+        user.address,
+      ) &&
       [
         Milestone.PROPOSED,
         Milestone.REJECTED,
         Milestone.IN_PROGRESS,
         Milestone.NEEDS_REVIEW,
-      ].includes(this.status)
+      ].includes(this.status) &&
+      this.totalDonations <= 0
     );
   }
 
