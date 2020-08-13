7210479b651a0e9093ccb65baa2f928b6e5497a2
horus2021 <khanih@gmail.com>
2020-08-13 13:44:08 +0430

    Fix issue 1349 Ask Anon Donors to fill out a profile 3rd try

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -28,7 +28,7 @@ import RangeSlider from './RangeSlider';
 import NumericInput from './NumericInput';
 import getWeb3 from '../lib/blockchain/getWeb3';
 import ExchangeButton from './ExchangeButton';
-import { authenticateIfPossible, checkProfileAfterDonation } from '../lib/middleware';
+import { checkProfileAfterDonation } from '../lib/middleware';
 
 const POLL_DELAY_TOKENS = 2000;
 const UPDATE_ALLOWANCE_DELAY = 1000; // Delay allowance update inorder to network respond new value
@@ -1062,11 +1062,6 @@ export default class Root extends React.PureComponent {
   }
 
   afterSuccessfulDonate() {
-    // anon user (without profile)
-    authenticateIfPossible(this.props.currentUser, true).then(() =>
-      checkProfileAfterDonation(this.props.currentUser),
-    );
-
     // known user (with profile)
     const { donateToDefaultDac } = this.state;
 
@@ -1082,6 +1077,9 @@ export default class Root extends React.PureComponent {
         }
       });
     }
+
+    // anon user (without profile)
+    checkProfileAfterDonation(this.props.currentUser);
   }
 
   render() {
