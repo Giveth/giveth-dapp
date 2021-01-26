aea7c7ded01144d65825ee73f1b4306312c97828
Amin Latifi <a.latifi.al@gmail.com>
2021-01-26 04:14:03 +0330

    Change requested amount number format in milestone footer.

--- src/components/CardStats.jsx
+++ src/components/CardStats.jsx
@@ -9,61 +9,66 @@ import config from '../configuration';
  *
  * TODO: Check the properties that are passed, sometimes they are number, sometimes strings...
  */
-const CardStats = ({ peopleCount, maxAmount, totalDonated, type, status, token }) => (
-  <div className="row card-stats">
-    {['dac', 'campaign'].includes(type) && (
-      <div className="col-6 text-left">
-        <p>Giver(s)</p>
-        <span>
-          <i className="fa fa-male" />
-          {peopleCount}
-        </span>
-      </div>
-    )}
+const CardStats = ({ peopleCount, maxAmount, totalDonated, type, status, token }) => {
+  let amountRequested = new BigNumber(convertEthHelper(maxAmount, token.decimals));
+  amountRequested = amountRequested.toPrecision(8).replace(/0+$/, '');
 
-    {type === 'milestone' && (
-      <div className="col-3 text-left">
-        <p>Giver(s)</p>
-        <span>
-          <i className="fa fa-male" />
-          {peopleCount}
-        </span>
-      </div>
-    )}
-
-    {['dac', 'campaign'].includes(type) && (
-      <div className="col-5 text-center card-center">
-        <span>
-          <p>Donations</p>
-          <p>{totalDonated.toFixed()}</p>
-        </span>
-      </div>
-    )}
-
-    {type === 'milestone' && (
-      <div className="col-5 text-center card-center">
-        {maxAmount && (
+  return (
+    <div className="row card-stats">
+      {['dac', 'campaign'].includes(type) && (
+        <div className="col-6 text-left">
+          <p>Giver(s)</p>
           <span>
-            <p>Requested</p>
-            <p>
-              {convertEthHelper(maxAmount, token.decimals)} {token.symbol}
-            </p>
+            <i className="fa fa-male" />
+            {peopleCount}
           </span>
-        )}
-      </div>
-    )}
+        </div>
+      )}
 
-    {type === 'milestone' && (
-      <div className="col-4 text-right">
-        <p>status</p>
-        <span>
-          <i className="fa fa-check-circle" />
-          {status}
-        </span>
-      </div>
-    )}
-  </div>
-);
+      {type === 'milestone' && (
+        <div className="col-3 text-left">
+          <p>Giver(s)</p>
+          <span>
+            <i className="fa fa-male" />
+            {peopleCount}
+          </span>
+        </div>
+      )}
+
+      {['dac', 'campaign'].includes(type) && (
+        <div className="col-5 text-center card-center">
+          <span>
+            <p>Donations</p>
+            <p>{totalDonated.toFixed()}</p>
+          </span>
+        </div>
+      )}
+
+      {type === 'milestone' && (
+        <div className="col-5 text-center card-center">
+          {maxAmount && (
+            <span>
+              <p>Requested</p>
+              <p>
+                {amountRequested} {token.symbol}
+              </p>
+            </span>
+          )}
+        </div>
+      )}
+
+      {type === 'milestone' && (
+        <div className="col-4 text-right">
+          <p>status</p>
+          <span>
+            <i className="fa fa-check-circle" />
+            {status}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
 
 CardStats.propTypes = {
   type: PropTypes.string.isRequired,
