a3bcd444e229af8153cb995502bc8f4516209abc
Amin Latifi <a.latifi.al@gmail.com>
2021-02-02 15:19:52 +0330

    Make cards to function components

--- src/components/DacCard.jsx
+++ src/components/DacCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -9,70 +9,51 @@ import GivethLogo from '../assets/logo.svg';
 
 /**
  * DAC Card visible in the DACs view.
- *
- * @param currentUser  Currently logged in user information
  */
-class DacCard extends Component {
-  constructor(props) {
-    super(props);
+const DacCard = props => {
+  const viewDAC = () => {
+    history.push(`/dacs/${props.dac.id}`);
+  };
 
-    this.viewDAC = this.viewDAC.bind(this);
-    this.createDACLink = this.createDACLink.bind(this);
-  }
+  const createDACLink = () => {
+    return `/dacs/${props.dac.id}`;
+  };
 
-  viewDAC() {
-    history.push(`/dacs/${this.props.dac.id}`);
-  }
+  const { dac } = props;
+  const colors = ['#76318f', '#50b0cf', '#1a1588', '#2A6813', '#95d114', '#155388', '#604a7d'];
+  const color = colors[Math.floor(Math.random() * colors.length)];
 
-  createDACLink() {
-    return `/dacs/${this.props.dac.id}`;
-  }
+  return (
+    <div className="card overview-card" id={dac.id} onKeyPress={viewDAC} role="button" tabIndex="0">
+      <Link className="card-body" to={createDACLink()}>
+        <div
+          className="card-img"
+          style={{
+            backgroundColor: dac.image ? 'white' : color,
+            backgroundImage: `url(${dac.image || GivethLogo})`,
+          }}
+        />
 
-  render() {
-    const { dac } = this.props;
-    const colors = ['#76318f', '#50b0cf', '#1a1588', '#2A6813', '#95d114', '#155388', '#604a7d'];
-    const color = colors[Math.floor(Math.random() * colors.length)];
+        <div className="card-content">
+          <h4 className="card-title">{getTruncatedText(dac.title, 30)}</h4>
+          <div className="card-text">{dac.summary}</div>
+        </div>
 
-    return (
-      <div
-        className="card overview-card"
-        id={dac.id}
-        onKeyPress={this.viewDAC}
-        role="button"
-        tabIndex="0"
-      >
-        <Link className="card-body" to={this.createDACLink()}>
-          <div
-            className="card-img"
-            style={{
-              backgroundColor: dac.image ? 'white' : color,
-              backgroundImage: `url(${dac.image || GivethLogo})`,
-            }}
+        <div className="card-footer">
+          <CardStats
+            type="dac"
+            peopleCount={dac.peopleCount}
+            totalDonated={dac.totalDonationCount}
+            currentBalance={dac.currentBalance}
           />
-
-          <div className="card-content">
-            <h4 className="card-title">{getTruncatedText(dac.title, 30)}</h4>
-            <div className="card-text">{dac.summary}</div>
-          </div>
-
-          <div className="card-footer">
-            <CardStats
-              type="dac"
-              peopleCount={dac.peopleCount}
-              totalDonated={dac.totalDonationCount}
-              currentBalance={dac.currentBalance}
-            />
-          </div>
-        </Link>
-      </div>
-    );
-  }
-}
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 DacCard.propTypes = {
   dac: PropTypes.instanceOf(DAC).isRequired,
 };
 
-DacCard.defaultProps = {};
-
-export default DacCard;
+export default React.memo(DacCard);
