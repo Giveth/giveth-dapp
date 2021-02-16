ee51fb112eec4ab4d5bc13db47199728122a6f2e
pourcheriki <pourcheriki@gmail.com>
2021-02-16 15:30:54 +0330

    add multiple expenses

--- src/components/views/CreateExpense.jsx
+++ src/components/views/CreateExpense.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { PageHeader, Row, Col, Form, Input, Upload, Select, Button, DatePicker } from 'antd';
+import { PageHeader, Row, Col, Form, Input, Select, Button } from 'antd';
 import 'antd/dist/antd.css';
 import PropTypes from 'prop-types';
-import ImgCrop from 'antd-img-crop';
+import { v4 as uuidv4 } from 'uuid';
 import CampaignService from '../../services/CampaignService';
+import ExpenseCreateForm from '../ExpenseCreateForm';
 
 class CreateExpense extends Component {
   constructor(props) {
@@ -27,20 +28,24 @@ class CreateExpense extends Component {
     ];
     this.state = {
       campaign: {},
-      amount: undefined,
-      currency: undefined,
-      date: undefined,
-      description: '',
-      picture: '',
+      expenses: [
+        {
+          amount: '',
+          currency: '',
+          date: '',
+          description: '',
+          picture: '',
+          key: uuidv4(),
+        },
+      ],
       reimbursementCurrency: undefined,
       wallet: undefined,
     };
     this.goBack = this.goBack.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.toggleHasReviewer = this.toggleHasReviewer.bind(this);
-    this.handleSelectCurrency = this.handleSelectCurrency.bind(this);
     this.handleSelectReimbursementCurrency = this.handleSelectReimbursementCurrency.bind(this);
-    this.handleDatePicker = this.handleDatePicker.bind(this);
+    this.addExpense = this.addExpense.bind(this);
+    this.updateStateOfexpenses = this.updateStateOfexpenses.bind(this);
   }
 
   async componentDidMount() {
@@ -59,26 +64,37 @@ class CreateExpense extends Component {
     const { target } = event;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const { name } = target;
-
     this.setState({
       [name]: value,
     });
   }
 
-  toggleHasReviewer(checked) {
-    this.setState({ hasReviewer: checked });
-  }
+  updateStateOfexpenses(name, value, expKey) {
+    const expenses = [...this.state.expenses];
+    const expense = expenses.find(exp => exp.key === expKey);
+    expense[name] = value;
 
-  handleSelectCurrency(_, option) {
-    this.setState({ currency: option.value });
+    this.setState({ expenses: [...expenses] });
   }
 
   handleSelectReimbursementCurrency(_, option) {
     this.setState({ reimbursementCurrency: option.value });
   }
 
-  handleDatePicker(_, dateString) {
-    this.setState({ date: dateString });
+  addExpense() {
+    this.setState(prevState => ({
+      expenses: [
+        ...prevState.expenses,
+        {
+          amount: '',
+          currency: '',
+          date: '',
+          description: '',
+          picture: '',
+          key: uuidv4(),
+        },
+      ],
+    }));
   }
 
   goBack() {
@@ -88,46 +104,11 @@ class CreateExpense extends Component {
   render() {
     const {
       campaign,
-      amount,
-      currency,
-      date,
-      description,
+      expenses,
       // picture,
       reimbursementCurrency,
       wallet,
     } = this.state;
-    const self = this;
-    const uploadProps = {
-      multiple: false,
-      accept: 'image/png, image/jpeg',
-      fileList: [],
-      customRequest: options => {
-        const { onSuccess, onError, file, onProgress } = options;
-        console.log(file);
-        onProgress(0);
-        if (true) {
-          // upload to ipfs
-          onSuccess('ipfs Address');
-          onProgress(100);
-        } else {
-          onError('Failed!');
-        }
-      },
-      onChange(info) {
-        console.log('info', info);
-        const { status } = info.file;
-        if (status !== 'uploading') {
-          console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-          console.log(`${info.file.name} file uploaded successfully.`);
-          self.setState({ picture: info.file.response });
-        } else if (status === 'error') {
-          console.log(`${info.file.name} file upload failed.`);
-        }
-      },
-    };
-
     return (
       <div id="create-expense-view">
         <Row>
@@ -150,86 +131,16 @@ class CreateExpense extends Component {
                 <div className="lable">Campaign</div>
                 <div className="content">{campaign.title}</div>
               </div>
-              <div className="section">
-                <div className="title">Expense details</div>
-                <Row gutter={16}>
-                  <Col className="gutter-row" span={10}>
-                    <Form.Item name="amount" label="Amount" className="custom-form-item">
-                      <Input
-                        value={amount}
-                        name="amount"
-                        type="number"
-                        placeholder="Enter Amount"
-                        onChange={this.handleInputChange}
-                        required
-                      />
-                    </Form.Item>
-                    <div className="form-item-desc">
-                      The amount should be the same as on the receipt.
-                    </div>
-                  </Col>
-                  <Col className="gutter-row" span={10}>
-                    <Form.Item name="currency" label="Currency" className="custom-form-item">
-                      <Select
-                        showSearch
-                        placeholder="Select a Currency"
-                        optionFilterProp="children"
-                        name="currency"
-                        onSelect={this.handleSelectCurrency}
-                        filterOption={(input, option) =>
-                          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                        }
-                        value={currency}
-                        required
-                      >
-                        {this.currencies.map(cur => (
-                          <Select.Option key={cur} value={cur}>
-                            {cur}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                    </Form.Item>
-                    <div className="form-item-desc">Select the currency of this expense.</div>
-                  </Col>
-                </Row>
-                <Row gutter={16}>
-                  <Col className="gutter-row" span={10}>
-                    <Form.Item name="date" label="Date" className="custom-form-item">
-                      <DatePicker onChange={this.handleDatePicker} value={date} />
-                    </Form.Item>
-                  </Col>
-                </Row>
-                <Form.Item
-                  name="description"
-                  label="Description of the expense"
-                  className="custom-form-item"
-                >
-                  <Input.TextArea
-                    value={description}
-                    name="description"
-                    placeholder="e.g. Lunch"
-                    onChange={this.handleInputChange}
-                    required
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="picture"
-                  label="Add a picture (optional)"
-                  className="custom-form-item"
-                >
-                  <ImgCrop>
-                    <Upload.Dragger {...uploadProps}>
-                      <p className="ant-upload-text">
-                        Drag and Drop JPEG, PNG here or <span>Attach a file.</span>
-                      </p>
-                    </Upload.Dragger>
-                  </ImgCrop>
-                </Form.Item>
-                <div className="form-item-desc">
-                  A picture says more than a thousand words. Select a png or jpg file in a 1:1
-                  aspect ratio.
-                </div>
-              </div>
+              {expenses.map((expense, idx) => (
+                <ExpenseCreateForm
+                  key={expense.key}
+                  expense={expense}
+                  id={idx}
+                  updateStateOfexpenses={this.updateStateOfexpenses}
+                />
+              ))}
+              <Button onClick={this.addExpense}>Add new Expense</Button>
+
               <div className="section">
                 <div className="title">Reimbursement options</div>
                 <Form.Item
