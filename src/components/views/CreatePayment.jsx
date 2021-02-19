53856b77d518d055b50b27c43798f74b1493cf48
pourcheriki <pourcheriki@gmail.com>
2021-02-20 00:27:39 +0330

    change CreatePayment to FC

--- src/components/views/CreatePayment.jsx
+++ src/components/views/CreatePayment.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   PageHeader,
   Row,
@@ -16,294 +16,274 @@ import PropTypes from 'prop-types';
 import ImgCrop from 'antd-img-crop';
 import CampaignService from '../../services/CampaignService';
 
-class CreatePayment extends Component {
-  constructor(props) {
-    super(props);
-    this.currencies = [
-      'ETH',
-      'DAI',
-      'PAN',
-      'BTC',
-      'USDC',
-      'USD',
-      'AUD',
-      'BRL',
-      'CAD',
-      'CHF',
-      'CZK',
-      'EUR',
-      'GBP',
-      'MXN',
-      'THB',
-    ];
-    this.state = {
-      campaign: {},
-      amount: undefined,
-      currency: undefined,
-      paymentCurrency: undefined,
-      date: undefined,
-      description: '',
-      picture: '',
-      donate: true,
-      wallet: undefined,
-    };
-    this.goBack = this.goBack.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSelectCurrency = this.handleSelectCurrency.bind(this);
-    this.handleSelectPaymentCurrency = this.handleSelectPaymentCurrency.bind(this);
-    this.handleDatePicker = this.handleDatePicker.bind(this);
+function CreatePayment(props) {
+  const currencies = [
+    'ETH',
+    'DAI',
+    'PAN',
+    'BTC',
+    'USDC',
+    'USD',
+    'AUD',
+    'BRL',
+    'CAD',
+    'CHF',
+    'CZK',
+    'EUR',
+    'GBP',
+    'MXN',
+    'THB',
+  ];
+
+  const [campaign, setCampaign] = useState();
+  const [payment, setPayment] = useState({
+    amount: '',
+    currency: '',
+    paymentCurrency: '',
+    date: '',
+    description: '',
+    picture: '',
+    donate: true,
+    wallet: '',
+  });
+
+  useEffect(async () => {
+    async function getCampaign() {
+      const campaignId = props.match.params.id;
+      const camp = await CampaignService.get(campaignId);
+      setCampaign(camp);
+    }
+    getCampaign();
+  }, []);
+
+  const handleInputChange = event => {
+    const { name, value, type, checked } = event.target;
+    if (type === 'checkbox') {
+      setPayment({ ...payment, [name]: checked });
+    } else {
+      setPayment({ ...payment, [name]: value });
+    }
+  };
+
+  function handleSelectCurrency(_, option) {
+    handleInputChange({ target: { name: 'currency', value: option.value } });
   }
 
-  async componentDidMount() {
-    const campaignId = this.props.match.params.id;
-    const campaign = await CampaignService.get(campaignId);
-    this.setState({
-      campaign,
+  function handleSelectPaymentCurrency(_, option) {
+    handleInputChange({
+      target: { name: 'paymentCurrency', value: option.value },
     });
   }
 
-  componentDidUpdate() {
-    console.log(this.state);
+  function handleDatePicker(_, dateString) {
+    handleInputChange({ target: { name: 'date', value: dateString } });
   }
 
-  handleInputChange(event) {
-    const { target } = event;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const { name } = target;
-
-    this.setState({
-      [name]: value,
-    });
+  function setPicture(address) {
+    handleInputChange({ target: { name: 'picture', value: address } });
   }
 
-  handleSelectCurrency(_, option) {
-    this.setState({ currency: option.value });
+  function goBack() {
+    props.history.goBack();
   }
 
-  handleSelectPaymentCurrency(_, option) {
-    this.setState({ paymentCurrency: option.value });
-  }
+  const uploadProps = {
+    multiple: false,
+    accept: 'image/png, image/jpeg',
+    fileList: [],
+    customRequest: options => {
+      const { onSuccess, onError, file, onProgress } = options;
+      console.log(file);
+      onProgress(0);
+      if (true) {
+        // upload to ipfs
+        onSuccess('ipfs Address');
+        onProgress(100);
+      } else {
+        onError('Failed!');
+      }
+    },
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        console.log(`${info.file.name} file uploaded successfully.`);
+        setPicture(info.file.response);
+      } else if (status === 'error') {
+        console.log(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
 
-  handleDatePicker(_, dateString) {
-    this.setState({ date: dateString });
-  }
-
-  goBack() {
-    this.props.history.goBack();
-  }
-
-  render() {
-    const {
-      campaign,
-      amount,
-      currency,
-      date,
-      description,
-      // picture,
-      donate,
-      paymentCurrency,
-      wallet,
-    } = this.state;
-    const self = this;
-    const uploadProps = {
-      multiple: false,
-      accept: 'image/png, image/jpeg',
-      fileList: [],
-      customRequest: options => {
-        const { onSuccess, onError, file, onProgress } = options;
-        console.log(file);
-        onProgress(0);
-        if (true) {
-          // upload to ipfs
-          onSuccess('ipfs Address');
-          onProgress(100);
-        } else {
-          onError('Failed!');
-        }
-      },
-      onChange(info) {
-        console.log('info', info);
-        const { status } = info.file;
-        if (status !== 'uploading') {
-          console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-          console.log(`${info.file.name} file uploaded successfully.`);
-          self.setState({ picture: info.file.response });
-        } else if (status === 'error') {
-          console.log(`${info.file.name} file upload failed.`);
-        }
-      },
-    };
-
-    return (
-      <div id="create-payment-view">
-        <Row>
-          <Col span={24}>
-            <PageHeader
-              className="site-page-header my-test"
-              onBack={this.goBack}
-              title="Create New Payment"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <div className="card-form-container">
-            <Form
-              className="card-form"
-              initialValues={{
-                currency: this.currencies[0],
-              }}
-            >
-              <div className="card-form-header">
-                <img src={`${process.env.PUBLIC_URL}/img/payment.png`} alt="payment-logo" />
-                <div className="title">Payment</div>
-              </div>
-              <div className="campaign-info">
-                <div className="lable">Campaign</div>
-                <div className="content">{campaign.title}</div>
-              </div>
-              <div className="section">
-                <div className="title">Payment details</div>
-                <Row gutter={16}>
-                  <Col className="gutter-row" span={10}>
-                    <Form.Item name="amount" label="Amount" className="custom-form-item">
-                      <Input
-                        value={amount}
-                        name="amount"
-                        type="number"
-                        placeholder="Enter Amount"
-                        onChange={this.handleInputChange}
-                        required
-                      />
-                    </Form.Item>
-                  </Col>
-                  <Col className="gutter-row" span={10}>
-                    <Form.Item name="currency" label="Currency" className="custom-form-item">
-                      <Select
-                        showSearch
-                        placeholder="Select a Currency"
-                        optionFilterProp="children"
-                        name="currency"
-                        onSelect={this.handleSelectCurrency}
-                        filterOption={(input, option) =>
-                          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                        }
-                        value={currency}
-                        required
-                      >
-                        {this.currencies.map(cur => (
-                          <Select.Option key={cur} value={cur}>
-                            {cur}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                    </Form.Item>
-                    <div className="form-item-desc">Select the currency of this expense.</div>
-                  </Col>
-                </Row>
-                <Row gutter={16}>
-                  <Col className="gutter-row" span={10}>
-                    <Form.Item name="date" label="Date" className="custom-form-item">
-                      <DatePicker onChange={this.handleDatePicker} value={date} />
-                    </Form.Item>
-                  </Col>
-                </Row>
-                <Form.Item
+  return (
+    <div id="create-payment-view">
+      <Row>
+        <Col span={24}>
+          <PageHeader
+            className="site-page-header my-test"
+            onBack={goBack}
+            title="Create New Payment"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <div className="card-form-container">
+          <Form
+            className="card-form"
+            initialValues={{
+              currency: currencies[0],
+            }}
+          >
+            <div className="card-form-header">
+              <img src={`${process.env.PUBLIC_URL}/img/payment.png`} alt="payment-logo" />
+              <div className="title">Payment</div>
+            </div>
+            <div className="campaign-info">
+              <div className="lable">Campaign</div>
+              <div className="content">{campaign && campaign.title}</div>
+            </div>
+            <div className="section">
+              <div className="title">Payment details</div>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={10}>
+                  <Form.Item name="amount" label="Amount" className="custom-form-item">
+                    <Input
+                      value={payment.amount}
+                      name="amount"
+                      type="number"
+                      placeholder="Enter Amount"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </Form.Item>
+                </Col>
+                <Col className="gutter-row" span={10}>
+                  <Form.Item name="currency" label="Currency" className="custom-form-item">
+                    <Select
+                      showSearch
+                      placeholder="Select a Currency"
+                      optionFilterProp="children"
+                      name="currency"
+                      onSelect={handleSelectCurrency}
+                      filterOption={(input, option) =>
+                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                      }
+                      value={payment.currency}
+                      required
+                    >
+                      {currencies.map(cur => (
+                        <Select.Option key={cur} value={cur}>
+                          {cur}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                  <div className="form-item-desc">Select the currency of this expense.</div>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={10}>
+                  <Form.Item name="date" label="Date" className="custom-form-item">
+                    <DatePicker onChange={handleDatePicker} value={payment.date} />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Form.Item
+                name="description"
+                label="Description of the payment request"
+                className="custom-form-item"
+              >
+                <Input.TextArea
+                  value={payment.description}
                   name="description"
-                  label="Description of the payment request"
-                  className="custom-form-item"
-                >
-                  <Input.TextArea
-                    value={description}
-                    name="description"
-                    placeholder="e.g. Monthly salary"
-                    onChange={this.handleInputChange}
-                    required
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="picture"
-                  label="Add a picture (optional)"
-                  className="custom-form-item"
-                >
-                  <ImgCrop>
-                    <Upload.Dragger {...uploadProps}>
-                      <p className="ant-upload-text">
-                        Drag and Drop JPEG, PNG here or <span>Attach a file.</span>
-                      </p>
-                    </Upload.Dragger>
-                  </ImgCrop>
-                </Form.Item>
-                <div className="form-item-desc">
-                  A picture says more than a thousand words. Select a png or jpg file in a 1:1
-                  aspect ratio.
-                </div>
-                <Form.Item name="donate" className="custom-form-item donate-giveth">
-                  <Checkbox onChange={this.handleInputChange} name="donate" checked={donate}>
-                    Donate 3% to Giveth
-                  </Checkbox>
-                </Form.Item>
-                <div className="form-item-desc">
-                  Your help keeps Giveth alive.
-                  <span role="img" aria-label="heart">
-                    {' '}
-                    ❤️
-                  </span>
-                </div>
-              </div>
-              <div className="section">
-                <div className="title">Payment options</div>
-                <Form.Item
-                  name="paymentCurrency"
-                  label="Payment currency"
-                  className="custom-form-item"
-                >
-                  <Select
-                    showSearch
-                    placeholder="Select a Currency"
-                    optionFilterProp="children"
-                    name="paymentCurrency"
-                    onSelect={this.handleSelectPaymentCurrency}
-                    filterOption={(input, option) =>
-                      option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                    }
-                    value={paymentCurrency}
-                    required
-                  >
-                    {this.currencies.map(cur => (
-                      <Select.Option key={cur} value={cur}>
-                        {cur}
-                      </Select.Option>
-                    ))}
-                  </Select>
-                </Form.Item>
-                <div className="form-item-desc">Select the token you want to be reimbursed in.</div>
-                <Form.Item name="wallet" label="Pay to wallet address" className="custom-form-item">
-                  <Input
-                    value={wallet}
-                    name="wallet"
-                    placeholder="0x"
-                    onChange={this.handleInputChange}
-                    required
-                  />
-                </Form.Item>
-                <div className="form-item-desc">
-                  If you don’t change this field the address associated with your account will be
-                  used.
-                </div>
-              </div>
-              <Form.Item>
-                <Button type="primary" htmlType="submit" className="submit-button">
-                  Submit
-                </Button>
+                  placeholder="e.g. Monthly salary"
+                  onChange={handleInputChange}
+                  required
+                />
               </Form.Item>
-            </Form>
-          </div>
-        </Row>
-      </div>
-    );
-  }
+              <Form.Item
+                name="picture"
+                label="Add a picture (optional)"
+                className="custom-form-item"
+              >
+                <ImgCrop>
+                  <Upload.Dragger {...uploadProps}>
+                    <p className="ant-upload-text">
+                      Drag and Drop JPEG, PNG here or <span>Attach a file.</span>
+                    </p>
+                  </Upload.Dragger>
+                </ImgCrop>
+              </Form.Item>
+              <div className="form-item-desc">
+                A picture says more than a thousand words. Select a png or jpg file in a 1:1 aspect
+                ratio.
+              </div>
+              <Form.Item name="donate" className="custom-form-item donate-giveth">
+                <Checkbox onChange={handleInputChange} name="donate" checked={payment.donate}>
+                  Donate 3% to Giveth
+                </Checkbox>
+              </Form.Item>
+              <div className="form-item-desc">
+                Your help keeps Giveth alive.
+                <span role="img" aria-label="heart">
+                  {' '}
+                  ❤️
+                </span>
+              </div>
+            </div>
+            <div className="section">
+              <div className="title">Payment options</div>
+              <Form.Item
+                name="paymentCurrency"
+                label="Payment currency"
+                className="custom-form-item"
+              >
+                <Select
+                  showSearch
+                  placeholder="Select a Currency"
+                  optionFilterProp="children"
+                  name="paymentCurrency"
+                  onSelect={handleSelectPaymentCurrency}
+                  filterOption={(input, option) =>
+                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                  }
+                  value={payment.paymentCurrency}
+                  required
+                >
+                  {currencies.map(cur => (
+                    <Select.Option key={cur} value={cur}>
+                      {cur}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </Form.Item>
+              <div className="form-item-desc">Select the token you want to be reimbursed in.</div>
+              <Form.Item name="wallet" label="Pay to wallet address" className="custom-form-item">
+                <Input
+                  value={payment.wallet}
+                  name="wallet"
+                  placeholder="0x"
+                  onChange={handleInputChange}
+                  required
+                />
+              </Form.Item>
+              <div className="form-item-desc">
+                If you don’t change this field the address associated with your account will be
+                used.
+              </div>
+            </div>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="submit-button">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Row>
+    </div>
+  );
 }
 
 CreatePayment.propTypes = {
