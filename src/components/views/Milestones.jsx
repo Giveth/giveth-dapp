0dfbcbc683ec89b42e2b9d95ca381ef03469b83a
pourcheriki <pourcheriki@gmail.com>
2021-02-24 01:44:42 +0330

    add width limit to firstpage cards

--- src/components/views/Milestones.jsx
+++ src/components/views/Milestones.jsx
@@ -47,47 +47,49 @@ const Milestones = ({ step }) => {
   }, []);
 
   return (
-    <div id="milestones-view" className="card-view">
-      <div className="container-fluid page-layout reduced-padding">
-        <h4>Milestones {total > 0 && <span className="badge badge-success">{total}</span>}</h4>
-        {!hasError && milestones.length > 0 && (
-          <div>
+    <div className="container">
+      <div id="milestones-view" className="card-view">
+        <div className="container-fluid page-layout reduced-padding">
+          <h4>Milestones {total > 0 && <span className="badge badge-success">{total}</span>}</h4>
+          {!hasError && milestones.length > 0 && (
+            <div>
+              <p>
+                Here are a few of the latest Milestones. Help them realise their goals by giving
+                Ether or tokens!
+              </p>
+              <div className="cards-grid-container">
+                {milestones.map(milestone => (
+                  <MilestoneCard key={milestone.id} milestone={milestone} />
+                ))}
+              </div>
+              {milestones.length < total && <LoadMore onClick={loadMore} disabled={isLoading} />}
+            </div>
+          )}
+
+          {!hasError && isLoading && <Loader />}
+
+          {// There are no Milestones, show empty state
+          !hasError && !isLoading && milestones.length === 0 && (
+            <div>
+              <div className="text-center">
+                <p>There are no Milestones yet!</p>
+                <img
+                  className="empty-state-img"
+                  src={`${process.env.PUBLIC_URL}/img/milestone.svg`}
+                  width="200px"
+                  height="200px"
+                  alt="no-milestones-icon"
+                />
+              </div>
+            </div>
+          )}
+          {hasError && (
             <p>
-              Here are a few of the latest Milestones. Help them realise their goals by giving Ether
-              or tokens!
+              <strong>Oops, something went wrong...</strong> The Giveth dapp could not load
+              Milestones for some reason. Please try refreshing the page...
             </p>
-            <div className="cards-grid-container">
-              {milestones.map(milestone => (
-                <MilestoneCard key={milestone.id} milestone={milestone} />
-              ))}
-            </div>
-            {milestones.length < total && <LoadMore onClick={loadMore} disabled={isLoading} />}
-          </div>
-        )}
-
-        {!hasError && isLoading && <Loader />}
-
-        {// There are no Milestones, show empty state
-        !hasError && !isLoading && milestones.length === 0 && (
-          <div>
-            <div className="text-center">
-              <p>There are no Milestones yet!</p>
-              <img
-                className="empty-state-img"
-                src={`${process.env.PUBLIC_URL}/img/milestone.svg`}
-                width="200px"
-                height="200px"
-                alt="no-milestones-icon"
-              />
-            </div>
-          </div>
-        )}
-        {hasError && (
-          <p>
-            <strong>Oops, something went wrong...</strong> The Giveth dapp could not load Milestones
-            for some reason. Please try refreshing the page...
-          </p>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
