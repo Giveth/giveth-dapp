383b561474a0ff6ad86e875c525e0b6837bf664b
Pourcheriki <pourcheriki@gmail.com>
2021-04-11 19:32:28 +0430

    add options to scrollToFirstError in bounty form

--- src/components/views/CreateBounty.jsx
+++ src/components/views/CreateBounty.jsx
@@ -170,7 +170,15 @@ function CreateBounty(props) {
         </Row>
         <Row>
           <div className="card-form-container">
-            <Form className="card-form" requiredMark onFinish={submit} scrollToFirstError>
+            <Form
+              className="card-form"
+              requiredMark
+              onFinish={submit}
+              scrollToFirstError={{
+                block: 'center',
+                behavior: 'smooth',
+              }}
+            >
               <div className="card-form-header">
                 <img src={`${process.env.PUBLIC_URL}/img/bounty.png`} alt="bounty-logo" />
                 <div className="title">Bounty</div>
