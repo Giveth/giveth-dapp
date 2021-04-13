a3db6a56200f4347a314842d33d26e357b7a6a1d
Pourcheriki <pourcheriki@gmail.com>
2021-04-13 23:57:17 +0430

    add scroll to top

--- src/components/views/MilestoneCreateOptionsMenu.jsx
+++ src/components/views/MilestoneCreateOptionsMenu.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Col, PageHeader, Row } from 'antd';
 import { history } from '../../lib/helpers';
 
 function MilestoneCreateOptionsMenu() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   function goBack() {
     history.goBack();
   }
