6730f3659ea960ac86388f38a024879f949d9bb3
Pourcheriki <pourcheriki@gmail.com>
2021-04-21 03:23:55 +0430

    add new policy to video upload

--- src/components/VideoPopup.jsx
+++ src/components/VideoPopup.jsx
@@ -1,201 +1,143 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useRef, useState } from 'react';
-import { Radio, Input, Modal, Button, notification } from 'antd';
+import React, { useState } from 'react';
+import { Modal, Form, Input, Radio, Upload, notification } from 'antd';
 import IPFSService from '../services/IPFSService';
-import config from '../configuration';
 
-const VideoPopup = ({ visible, handleClose, reactQuillRef }) => {
+const VideoPopup = ({ visible, handleClose }) => {
   const [type, setType] = useState(1);
-  const [url, setURL] = useState('');
-  const [youtubeUrl, setYoutubeURL] = useState('');
-  const [currentState, setCurrentState] = useState('');
-  const [loading, setLoading] = useState(false);
-  const stream = useRef(null);
-  const file = useRef(null);
+  const [url, setURL] = useState(1);
+  const [fileList, setFileList] = useState([]);
+  const [form] = Form.useForm();
 
-  const onChange = e => {
+  const onTypeChange = e => {
     setType(e.target.value);
   };
 
-  const handleCamera = () => {
-    navigator.getUserMedia(
-      { audio: true, video: true },
-      cameraStream => {
-        stream.current = new window.MultiStreamsMixer([cameraStream]);
-        stream.current.frameInterval = 1;
-        stream.current.startDrawingFrames();
-        window.setSrcObject(stream.current.getMixedStream(), document.getElementById('video'));
-        // this.setState(
-        //   {
-        //     stream: new window.MultiStreamsMixer([cameraStream]),
-        //     cameraStream,
-        //   },
-        //   () => {
-        //     this.state.stream.frameInterval = 1; // eslint-disable-line react/no-direct-mutation-state
-        //     this.state.stream.startDrawingFrames();
-        //     window.setSrcObject(
-        //       this.state.stream.getMixedStream(),
-        //       document.getElementById('video'),
-        //     );
-        //   },
-        // );
-      },
-      () => {
-        // alert('No camera devices found');
-      },
-    );
-  };
-
-  const detectExtension = () => {
-    const extensionid = 'ajhifddimkapgcifgcodmmfdlknahffk';
-    const image = document.createElement('img');
-    image.src = `chrome-extension://${extensionid}/icon.png`;
-
-    image.onload = () => {
-      // handleScreenSharing();
-    };
-    image.onerror = () => {
-      setCurrentState('missing extension');
-    };
-  };
-
-  useEffect(() => {
-    if (type === 4) {
-      handleCamera();
-    } else if (type === 5) {
-      detectExtension();
-    }
-  }, [type]);
-  function clearStates() {
-    if (file.current) {
-      file.current.value = null;
-    }
-    setLoading(false);
-    setURL(false);
-  }
-
-  function closeModal() {
-    clearStates();
-    handleClose();
-  }
-
-  function insertToEditor(videURL) {
-    // TODO: Remove This if after edit milestone conrt to FC;
-    let quill;
-    if (reactQuillRef.current) {
-      quill = reactQuillRef.current.getEditor();
-    } else {
-      quill = reactQuillRef.getEditor();
-    }
-    const index = quill.getLength() - 1;
-    quill.insertEmbed(index, 'video', videURL);
-  }
-
-  function getVideoUrl(tempUrl) {
-    let match =
-      tempUrl.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtube\.com\/watch.*v=([a-zA-Z0-9_-]+)/) ||
-      tempUrl.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtu\.be\/([a-zA-Z0-9_-]+)/) ||
-      tempUrl.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#&?]*).*/);
-    if (match && match[2].length === 11) {
-      return `https://www.youtube.com/embed/${match[2]}?showinfo=0`;
-    }
-    match = tempUrl.match(/^(?:(https?):\/\/)?(?:www\.)?vimeo\.com\/(\d+)/);
-    if (match) {
-      return `${match[1] || 'https'}://player.vimeo.com/video/${match[2]}/`;
-    }
-    return null;
-  }
-
-  function uploadVideoToIPFS() {
-    const reader = new FileReader();
-    reader.onload = _ => {
-      IPFSService.upload(file.current.files[0])
-        .then(hash => {
-          insertToEditor(config.ipfsGateway + hash.slice(6));
-          closeModal();
+  const uploadProps = {
+    multiple: false,
+    maxCount: 1,
+    accept: '.mp4',
+    fileList,
+    customRequest: options => {
+      const { onSuccess, onError, file, onProgress } = options;
+      onProgress(0);
+      IPFSService.upload(file)
+        .then(address => {
+          onSuccess(address);
+          onProgress(100);
         })
-        .catch(() => {
-          notification.error({
-            message: 'IPFS Fails',
-            description: 'Something went wrong with the upload.',
-          });
-        })
-        .finally(() => {
-          setLoading(false);
+        .catch(err => {
+          onError('Failed!', err);
         });
-    };
-    setLoading(true);
-    reader.readAsDataURL(file.current.files[0]);
-  }
-
-  function onOk() {
-    let tempURL;
-    switch (type) {
-      case 1:
-        insertToEditor(url);
-        closeModal();
-        break;
-      case 2:
-        uploadVideoToIPFS();
-        break;
-      case 3:
-        tempURL = getVideoUrl(youtubeUrl);
-        if (tempURL) {
-          insertToEditor(tempURL);
-          closeModal();
-        }
-        break;
-      default:
-        break;
-    }
-  }
+    },
+    onChange(info) {
+      const { status } = info.file;
+      setFileList(info.fileList);
+      if (status === 'uploading') {
+        console.log(info.fileList);
+      }
+      if (status === 'done') {
+        console.log('info.fileList :>> ', info.fileList);
+      } else if (status === 'error') {
+        console.log(`${info.file.name} file upload failed.`);
+        const args = {
+          message: 'Error',
+          description: 'Cannot upload picture to IPFS',
+        };
+        notification.error(args);
+      }
+    },
+  };
 
   return (
     <Modal
-      title="Attach a video to description"
       visible={visible}
-      onOk={onOk}
-      onCancel={closeModal}
-      footer={[
-        <Button key="back" onClick={closeModal}>
-          cancel
-        </Button>,
-        <Button key="submit" type="primary" loading={loading} onClick={onOk}>
-          Submit
-        </Button>,
-      ]}
+      title="Attach a video to description"
+      okText="Add"
+      cancelText="Cancel"
+      onCancel={handleClose}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(_ => {
+            // form.resetFields();
+            // onCreate(values);
+          })
+          .catch(info => {
+            console.log('Validate Failed:', info);
+          });
+      }}
     >
-      <Radio.Group onChange={onChange} value={type}>
-        <Radio value={1}>Link</Radio>
-        <Radio value={2}>File</Radio>
-        <Radio value={3}>Youtube</Radio>
-        {/* <Radio value={3}>Camera</Radio> */}
-        {/* <Radio value={4}>Screen sharing</Radio> */}
-      </Radio.Group>
-      <div style={{ paddingTop: '40px' }}>
-        {type === 1 && <Input placeholder="Video URL" onChange={e => setURL(e.target.value)} />}
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{
+          type: 1,
+        }}
+      >
+        <Form.Item name="type" className="collection-create-form_last-form-item">
+          <Radio.Group onChange={onTypeChange} value={type}>
+            <Radio value={1}>Link</Radio>
+            <Radio value={2}>File</Radio>
+            <Radio value={3}>Youtube</Radio>
+          </Radio.Group>
+        </Form.Item>
+        {type === 1 && (
+          <Form.Item
+            name="URL"
+            rules={[
+              {
+                required: true,
+                type: 'string',
+                min: 10,
+                message: 'Please provide at least 10 characters',
+              },
+            ]}
+          >
+            <Input placeholder="Video URL" onChange={e => setURL(e.target.value)} value={url} />
+          </Form.Item>
+        )}
         {type === 2 && (
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <input type="file" accept="video/*" ref={file} />
-          </div>
+          <Form.Item
+            name="File"
+            rules={[
+              {
+                required: true,
+                message: 'Please upload video.',
+              },
+              () => ({
+                validator(_, value) {
+                  if (!value || !value.fileList || !value.fileList[0]) {
+                    return Promise.reject(new Error('Something wrong'));
+                  }
+                  if (value.fileList[0].status === 'uploading') {
+                    return Promise.reject(new Error('Please Wait to video upload Completely'));
+                  }
+                  if (value.fileList[0].status === 'error') {
+                    return Promise.reject(new Error('Something wrong'));
+                  }
+                  if (value.fileList[0].status === 'done' && value.fileList[0].response) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error('The two passwords that you entered do not match!'),
+                  );
+                },
+              }),
+            ]}
+          >
+            <Upload.Dragger {...uploadProps}>
+              <img
+                src="/img/ipfs-logo1.svg"
+                alt="ipfs"
+                style={{ width: '100px', padding: '20px' }}
+              />
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            </Upload.Dragger>
+          </Form.Item>
         )}
-        {type === 3 && (
-          <Input placeholder="Video URL" onChange={e => setYoutubeURL(e.target.value)} />
-        )}
-        {currentState === 'missing extension' && (
-          <div role="alert">
-            <strong>You need to install this </strong>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://chrome.google.com/webstore/detail/screen-capturing/ajhifddimkapgcifgcodmmfdlknahffk"
-            >
-              Chrome extension
-            </a>{' '}
-            <strong>and reload</strong>
-          </div>
-        )}
-      </div>
+      </Form>
     </Modal>
   );
 };
