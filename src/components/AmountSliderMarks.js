1fe9ba5b7e9466c90e5f8707a7f4487addfb4c7c
Amin Latifi <a.latifi.al@gmail.com>
2021-04-28 12:21:35 +0430

    Changed slider marks labels

--- src/components/AmountSliderMarks.js
+++ src/components/AmountSliderMarks.js
@@ -1,10 +1,15 @@
 const AmountSliderMarks = (maxAmount, decimals) => {
   const sliderMarks = {};
-  sliderMarks[0] = '0%';
+  sliderMarks[0] = {
+    style: {
+      color: '#f50',
+    },
+    label: '0%',
+  };
   sliderMarks[Number((maxAmount / 4).toFixed(decimals))] = '25%';
   sliderMarks[Number((maxAmount / 2).toFixed(decimals))] = '50%';
   sliderMarks[Number(((3 * maxAmount) / 4).toFixed(decimals))] = '75%';
-  sliderMarks[Number(maxAmount.toFixed(decimals))] = '100%';
+  sliderMarks[Number(maxAmount.toFixed(decimals))] = 'Max';
   return sliderMarks;
 };
 
