376921bb7fac6f4ab24dfc0cbb30f1778172321b
Ramin <r.ramazanpour@aut.ac.ir>
2021-06-30 16:24:35 +0430

    F #2319 Use Ant components in ChangeOwnershipButton

--- src/components/ChangeOwnershipButton.jsx
+++ src/components/ChangeOwnershipButton.jsx
@@ -1,42 +1,23 @@
-import React, { Fragment, useContext, useState, useRef } from 'react';
-import Modal from 'react-modal';
+import React, { Fragment, useContext, useState, useEffect } from 'react';
 import BigNumber from 'bignumber.js';
-import { Form } from 'formsy-react-components';
 import PropTypes from 'prop-types';
-import 'react-rangeslider/lib/index.css';
+import { Button, Modal, Select, Form } from 'antd';
 
 import Campaign from 'models/Campaign';
-import Trace from 'models/Trace';
-import { Button } from 'antd';
-import { ZERO_ADDRESS } from '../lib/helpers';
 import { isLoggedIn, checkBalance } from '../lib/middleware';
+import { ZERO_ADDRESS } from '../lib/helpers';
 import config from '../configuration';
-import SelectFormsy from './SelectFormsy';
 import ActionNetworkWarning from './ActionNetworkWarning';
 
 import CampaignService from '../services/CampaignService';
 import { Context as Web3Context } from '../contextProviders/Web3Provider';
 import { Context as WhiteListContext } from '../contextProviders/WhiteListProvider';
 import { Context as UserContext } from '../contextProviders/UserProvider';
-import BridgedTrace from '../models/BridgedTrace';
-import LPPCappedTrace from '../models/LPPCappedTrace';
-import LPTrace from '../models/LPTrace';
 
 BigNumber.config({ DECIMAL_PLACES: 18 });
-Modal.setAppElement('#root');
 
-const modalStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-20%',
-    transform: 'translate(-50%, -50%)',
-    boxShadow: '0 0 40px #ccc',
-    overflowY: 'scroll',
-  },
-};
+const noCoownerValue = '------ No co-owner ------';
+const noCoownerObject = { value: noCoownerValue, title: noCoownerValue };
 
 /**
  * Retrieves the oldest 100 donations that the user can delegate
@@ -56,11 +37,12 @@ const ChangeOwnershipButton = props => {
     state: { campaignManagers },
   } = useContext(WhiteListContext);
 
+  const { campaign } = props;
+
   const [isSaving, setSaving] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
-  const { campaign, trace } = props;
-
-  const form = useRef();
+  const [coownerAddress, setCoownerAddress] = useState();
+  const [formIsReady, setFormIsReady] = useState(false);
 
   const openDialog = () => {
     isLoggedIn(currentUser)
@@ -87,23 +69,39 @@ const ChangeOwnershipButton = props => {
       campaign,
       currentUser.address,
       campaign.ownerAddress,
-      campaign.coownerAddress,
+      coownerAddress === noCoownerObject.value ? ZERO_ADDRESS : coownerAddress,
       afterCreate,
       afterMined,
     );
   };
 
+  // Set initial value for select component
+  useEffect(() => {
+    if (campaignManagers.length > 0) {
+      if (campaign.coownerAddress > ZERO_ADDRESS) {
+        setCoownerAddress(campaign.coownerAddress);
+      }
+      setFormIsReady(true);
+    }
+  }, [campaignManagers]);
+
+  const coownerList = [
+    noCoownerObject,
+    ...campaignManagers.filter(item => item.value !== campaign.ownerAddress),
+  ];
+
   return (
     <Fragment>
-      <Button type="primary" block style={props.style} danger onClick={() => openDialog()}>
+      <Button type="primary" block danger onClick={() => openDialog()}>
         Change co-owner
       </Button>
       <Modal
-        isOpen={modalVisible}
-        style={modalStyles}
-        onRequestClose={() => {
-          setModalVisible(false);
-        }}
+        visible={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        footer={null}
+        destroyOnClose
+        centered
+        className="pb-0"
       >
         {!validProvider && (
           <div className="alert alert-warning">
@@ -118,55 +116,40 @@ const ChangeOwnershipButton = props => {
           />
         )}
         {isForeignNetwork && (
-          <Fragment>
+          <Form onFinish={submit}>
             <p>
-              Changing ownership for
-              {!trace && <strong> {campaign.title}</strong>}
-              {trace && <strong> {trace.title}</strong>}
+              Changing ownership for<strong> {campaign.title}</strong>
             </p>
-            <Form
-              onSubmit={submit}
-              layout="vertical"
-              ref={form}
-              mapping={inputs => {
-                campaign.coownerAddress = inputs.coownerAddress || ZERO_ADDRESS;
-              }}
-            >
-              <div>
-                <SelectFormsy
+            <div className="label mb-2">Select a new co-owner:</div>
+            {formIsReady && (
+              <Fragment>
+                <Select
                   name="coownerAddress"
-                  id="co-owner-select"
-                  label="Select a new co-owner"
-                  helpText="This person or smart contract will be co-owning your Campaign from now on."
-                  value={campaign.coownerAddress}
-                  cta="------ Select a co-owner ------"
-                  options={campaignManagers}
-                  validations="isEtherAddress"
-                  validationErrors={{
-                    isEtherAddress: 'Please select a co-owner.',
-                  }}
-                  required
-                />
-                <button
-                  className="btn btn-success"
-                  formNoValidate
-                  type="submit"
-                  disabled={isSaving || !isForeignNetwork}
+                  placeholder="------ Select a co-owner ------"
+                  value={coownerAddress}
+                  onChange={setCoownerAddress}
+                  style={{ width: '100%' }}
+                  className="mr-3 mb-3"
+                  defaultValue={coownerAddress}
                 >
-                  {isSaving ? 'Changing...' : 'Change ownership'}
-                </button>
-                <button
-                  className="btn btn-light float-right"
-                  type="button"
-                  onClick={() => {
-                    setModalVisible(false);
-                  }}
-                >
-                  Close
-                </button>
-              </div>
-            </Form>
-          </Fragment>
+                  {coownerList.map(item => (
+                    <Select.Option value={item.value} key={item.value}>
+                      {item.title}
+                    </Select.Option>
+                  ))}
+                </Select>
+                <p>This person or smart contract will be co-owning your Campaign from now on.</p>
+              </Fragment>
+            )}
+            <Button
+              type="primary"
+              size="large"
+              disabled={isSaving || !isForeignNetwork || !coownerAddress}
+              onClick={submit}
+            >
+              {isSaving ? 'Changing...' : 'Change ownership'}
+            </Button>
+          </Form>
         )}
       </Modal>
     </Fragment>
@@ -174,17 +157,7 @@ const ChangeOwnershipButton = props => {
 };
 
 ChangeOwnershipButton.propTypes = {
-  campaign: PropTypes.instanceOf(Campaign),
-  trace: PropTypes.oneOfType(
-    [Trace, BridgedTrace, LPPCappedTrace, LPTrace].map(PropTypes.instanceOf),
-  ),
-  style: PropTypes.shape(),
-};
-
-ChangeOwnershipButton.defaultProps = {
-  campaign: undefined,
-  trace: undefined,
-  style: {},
+  campaign: PropTypes.instanceOf(Campaign).isRequired,
 };
 
 export default React.memo(ChangeOwnershipButton);
