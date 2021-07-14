a5e52b8b7f30324d29f3d42ff8190d72e7d3d572
Ramin <r.ramazanpour@aut.ac.ir>
2021-07-14 17:32:58 +0430

    F #1590 implement new manage menubar

--- src/components/layout/MainMenu/index.jsx
+++ src/components/layout/MainMenu/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 
 import MainMenu from './MainMenu';
 import Banner from './Banner';
+import ManageMenu from './ManageMenu';
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const profileArray = [
+    '/my-traces',
+    '/my-donations',
+    '/my-delegations',
+    '/my-communities',
+    '/my-campaigns',
+  ];
   return (
     <div id="header">
       <Banner />
       <MainMenu />
+      {profileArray.includes(pathname) && <ManageMenu />}
     </div>
   );
 };
